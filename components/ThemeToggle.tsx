5ff551377bb7ebc9ed7a0a63aca08f7cf746723f
jongmin <pinkserial@gmail.com>
2023-07-06 13:33:19 +0900

    theme 적용

---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { Listbox } from "@headlessui/react";
+import clsx from "clsx";
+import { useTheme } from "next-themes";
+import { Fragment, useEffect, useState } from "react";
+
+const themes = [
+  { value: "light", label: "Light", icon: SunIcon },
+  { value: "dark", label: "Dark", icon: MoonIcon },
+  { value: "system", label: "System", icon: SystemIcon },
+];
+
+function SunIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      >
+        <circle cx="12" cy="12" r="4"></circle>
+        <path d="M12 2v2m0 16v2M4.93 4.93l1.41 1.41m11.32 11.32l1.41 1.41M2 12h2m16 0h2M6.34 17.66l-1.41 1.41M19.07 4.93l-1.41 1.41"></path>
+      </g>
+    </svg>
+  );
+}
+
+function MoonIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 3a6 6 0 0 0 9 9a9 9 0 1 1-9-9Z"
+      ></path>
+    </svg>
+  );
+}
+
+function SystemIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      >
+        <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"></path>
+        <circle cx="12" cy="12" r="3"></circle>
+      </g>
+    </svg>
+  );
+}
+
+export default function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <div className="relative">
+      <Listbox value={theme} onChange={setTheme}>
+        <Listbox.Button className="flex items-center" type="button">
+          <span className="flex dark:hidden justify-center items-center gap-2">
+            <SunIcon className="w-6 h-6" />
+            Light Mode
+          </span>
+          <span className="hidden dark:flex  justify-center items-center gap-2">
+            <MoonIcon className="w-6 h-6" />
+            Dark Mode
+          </span>
+        </Listbox.Button>
+        <Listbox.Options className="bg-card absolute top-full mt-4 w-full rounded-lg ring-1 ring-slate-900/10 shadow-lg overflow-hidden">
+          {themes.map(({ value, label, icon: Icon }) => (
+            <Listbox.Option key={value} value={value} as={Fragment}>
+              {({ active, selected }) => (
+                <li
+                  className={clsx(
+                    "py-2 px-2 flex items-center cursor-pointer gap-1",
+                    selected && "text-sky-500",
+                    active && "bg-slate-50 dark:bg-slate-600/30"
+                  )}
+                >
+                  <Icon className="w-6 h-6" />
+                  <span className="flex-1 text-center ">{label}</span>
+                </li>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Listbox>
+    </div>
+  );
+}
